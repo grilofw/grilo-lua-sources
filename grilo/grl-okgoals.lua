d2ac5e9b07c2fb483bea30c850a7df8fe9a02fe7
Bastien Nocera <hadess@hadess.net>
2015-03-06 19:48:02 +0100

    grilo: Add icon for OKGoals

--- grilo/grl-okgoals.lua
+++ grilo/grl-okgoals.lua
@@ -35,7 +35,8 @@ source = {
   supported_keys = { "id", "thumbnail", "title", "url", "mime-type", "external-url" },
   supported_media = 'video',
   auto_split_threshold = 33,
-  tags = { 'news', 'sport', 'net:internet' }
+  tags = { 'news', 'sport', 'net:internet' },
+  icon = 'resource:///org/gnome/grilo/plugins/okgoals/grilo/okgoals.png'
 }
 
 ------------------
